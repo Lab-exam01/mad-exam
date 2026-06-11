216eebdd0a35818c8b701db9fe8657f730fe4162
Lab-exam01 <212363583+Lab-exam01@users.noreply.github.com>
2026-06-11 17:17:18 +0530

    Update 11.java

--- 11/11.java
+++ 11/11.java
@@ -1,6 +1,17 @@
+import android.content.Intent; 
+import android.os.Bundle; 
+import android.view.View; 
+import android.widget.Button; 
+import android.widget.EditText; 
+import android.widget.Toast; 
+import androidx.activity.EdgeToEdge; 
+import androidx.appcompat.app.AppCompatActivity; 
+import androidx.core.graphics.Insets; 
+import androidx.core.view.ViewCompat; 
+import androidx.core.view.WindowInsetsCompat; 
 public class MainActivity extends AppCompatActivity { 
-EditText etTo, etSub, etMsg; 
-Button btSend; 
+EditText etTo, etSub, etMsg;
+  Button btSend; 
 String to, subject, message; 
 @Override 
 protected void onCreate(Bundle savedInstanceState) { 
