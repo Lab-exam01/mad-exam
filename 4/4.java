102e643e87f491d7ba1ad943f8ba736c91bc9f2b
Lab-exam01 <212363583+Lab-exam01@users.noreply.github.com>
2026-06-11 17:06:56 +0530

    Update 4.java

--- 4/4.java
+++ 4/4.java
@@ -1,3 +1,19 @@
+import android.content.Intent; 
+import android.net.Uri; 
+import android.os.Bundle; 
+import android.view.View; 
+import androidx.activity.EdgeToEdge; 
+import androidx.appcompat.app.AppCompatActivity; 
+import androidx.core.graphics.Insets; 
+import androidx.core.view.ViewCompat; 
+import androidx.core.view.WindowInsetsCompat; 
+public class MainActivity extends AppCompatActivity {
+  @Override 
+protected void onCreate(Bundle savedInstanceState) { 
+super.onCreate(savedInstanceState); 
+EdgeToEdge.enable(this); 
+setContentView(R.layout.activity_main); 
+} 
 public void onImplicitButtonClicked(View view) 
 { 
 Uri url=Uri.parse("https://www.google.com"); 
