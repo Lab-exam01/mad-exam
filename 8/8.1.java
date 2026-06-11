9aee6371f47b883195255015616ed9f74a473d4b
Lab-exam01 <212363583+Lab-exam01@users.noreply.github.com>
2026-06-11 17:13:59 +0530

    Update 8.1.java

--- 8/8.1.java
+++ 8/8.1.java
@@ -1,5 +1,18 @@
+import android.content.Context; 
+import android.content.Intent; 
+import android.content.SharedPreferences; 
+import android.os.Bundle; 
+import android.view.View; 
+import android.widget.Button; 
+import android.widget.EditText; 
+import android.widget.Toast; 
+import androidx.activity.EdgeToEdge; 
+import androidx.appcompat.app.AppCompatActivity; 
+import androidx.core.graphics.Insets; 
+import androidx.core.view.ViewCompat; 
+import androidx.core.view.WindowInsetsCompat; 
 public class MainActivity extends AppCompatActivity {
-Button btnsave,btnnext; 
+  Button btnsave,btnnext; 
 EditText etUserName,etPassword; 
 @Override 
 protected void onCreate(Bundle savedInstanceState) { 
@@ -32,4 +45,4 @@ startActivity(intent);
 } 
 }); 
 } 
-}
+} 
