2252444f52836f19b4de77c3f68d9b08e696130c
Lab-exam01 <212363583+Lab-exam01@users.noreply.github.com>
2026-06-11 17:10:10 +0530

    Update 6.java

--- 6/6.java
+++ 6/6.java
@@ -1,4 +1,27 @@
-Button sub;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Button sub;
         sub=findViewById(R.id.button);
         sub.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -16,3 +39,4 @@ Button sub;
     }
 
     }
+
