2ac06409bda5a764dd1ee3c42334c6a8574dee59
Lab-exam01 <212363583+Lab-exam01@users.noreply.github.com>
2026-06-11 17:14:37 +0530

    Update 8.2.java

--- 8/8.2.java
+++ 8/8.2.java
@@ -1,3 +1,14 @@
+import android.content.Context; 
+import android.content.SharedPreferences; 
+import android.os.Bundle; 
+import android.view.View; 
+import android.widget.Button; 
+import android.widget.EditText; 
+import androidx.activity.EdgeToEdge; 
+import androidx.appcompat.app.AppCompatActivity; 
+import androidx.core.graphics.Insets; 
+import androidx.core.view.ViewCompat; 
+import androidx.core.view.WindowInsetsCompat; 
 public class MainActivity2 extends AppCompatActivity { 
 Button btnFetch; 
 EditText etUserName,etPassword; 
@@ -22,4 +33,4 @@ etPassword.setText(password);
 } 
 }); 
 } 
-} 
+}
