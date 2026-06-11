cffe6f03cd7dfb230b58576e8efc52c9b45fc360
Lab-exam01 <212363583+Lab-exam01@users.noreply.github.com>
2026-06-11 17:04:09 +0530

    Update 3.java

--- 3/3.java
+++ 3/3.java
@@ -1,3 +1,13 @@
+import android.os.Bundle; 
+import android.view.View; 
+import android.widget.Button; 
+import android.widget.EditText; 
+import android.widget.Toast; 
+import androidx.activity.EdgeToEdge; 
+import androidx.appcompat.app.AppCompatActivity; 
+import androidx.core.graphics.Insets; 
+import androidx.core.view.ViewCompat; 
+import androidx.core.view.WindowInsetsCompat; 
 public class MainActivity extends AppCompatActivity { 
 private EditText editTextUsername,editTextPassword; 
 private Button buttonLogin; 
