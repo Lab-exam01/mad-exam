30826c4b2140208bdcc4915ecfdf6da602a7cdae
Lab-exam01 <212363583+Lab-exam01@users.noreply.github.com>
2026-06-11 17:03:31 +0530

    Update 2.java

--- 2/2.java
+++ 2/2.java
@@ -1,3 +1,19 @@
+import android.content.pm.ActivityInfo; 
+import android.os.Bundle; 
+import android.view.View; 
+import android.widget.Button; 
+import android.widget.Toast; 
+import androidx.activity.EdgeToEdge; 
+import androidx.appcompat.app.AppCompatActivity; 
+import androidx.core.graphics.Insets; 
+import androidx.core.view.ViewCompat; 
+import androidx.core.view.WindowInsetsCompat; 
+public class MainActivity extends AppCompatActivity { 
+@Override 
+protected void onCreate(Bundle savedInstanceState) { 
+super.onCreate(savedInstanceState); 
+EdgeToEdge.enable(this); 
+setContentView(R.layout.activity_main); 
 Button l,p; 
 l=findViewById(R.id.lan); 
 p=findViewById(R.id.por); 
@@ -16,4 +32,6 @@ setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 Toast.makeText(MainActivity.this, "Hey! We are in Portrait orientation", 
 Toast.LENGTH_SHORT).show(); 
 } 
-});
+}); 
+} 
+}
