4fc3e4461e16a9002697496179a1844c7ae370f4
Lab-exam01 <212363583+Lab-exam01@users.noreply.github.com>
2026-06-11 17:12:55 +0530

    Update 7.java

--- 7/7.java
+++ 7/7.java
@@ -1,4 +1,24 @@
-  public boolean onCreatePanelMenu(int featureId, @NonNull Menu menu) { 
+import android.content.Intent; 
+import android.os.Bundle; 
+import android.view.Menu; 
+import android.view.MenuInflater; 
+import android.view.MenuItem; 
+import android.widget.Toast; 
+import androidx.activity.EdgeToEdge; 
+import androidx.annotation.NonNull; 
+import androidx.appcompat.app.AppCompatActivity; 
+import androidx.core.graphics.Insets; 
+import androidx.core.view.ViewCompat; 
+import androidx.core.view.WindowInsetsCompat; 
+public class MainActivity extends AppCompatActivity { 
+@Override 
+protected void onCreate(Bundle savedInstanceState) { 
+super.onCreate(savedInstanceState); 
+EdgeToEdge.enable(this); 
+setContentView(R.layout.activity_main); 
+}
+    @Override 
+    public boolean onCreatePanelMenu(int featureId, @NonNull Menu menu) { 
         MenuInflater inflater=getMenuInflater(); 
         inflater.inflate(R.menu.menus,menu); 
         return true; 
